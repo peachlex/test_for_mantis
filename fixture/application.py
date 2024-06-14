4459da40dd8e2ec11b4d1867b16d47e259142fd3
peachlex <peachlexx@gmail.com>
2024-06-14 04:04:06 +0300

    add mail confirmation

--- fixture/application.py
+++ fixture/application.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from fixture.james import JamesHelper
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 class Application:
 
@@ -20,6 +22,8 @@ class Application:
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
         self.config = config
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
         self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
